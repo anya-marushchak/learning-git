5e5dc2d6f04914a89b0d993e6d9e361a51aac0ab
anya-marushchak <121021987+anya-marushchak@users.noreply.github.com>
2022-12-19 16:03:01 +0100

    Delete another method of task 2

--- main.py
+++ main.py
@@ -20,18 +20,4 @@ print(my_list[0])
 print(my_list[4])
 print(my_list[10:12])
 print()
-#another method of task 2 using append function
-my_list = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
-new_list = []
-new_list_2 = []
-for number in my_list:
-  new_list.append(my_list[1:4])
-  new_list.append(my_list[5:10])
-  new_list.append(my_list[-2:])
-  new_list_2.append(my_list[0])
-  new_list_2.append(my_list[4])
-  new_list_2.append(my_list[10:12])
-  break
-print(my_list)
-print(new_list)
-print(new_list_2)
+
